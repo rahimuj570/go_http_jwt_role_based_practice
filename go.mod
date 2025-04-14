aab4fa0abcb22f89319c4f8b5b335d94ad6b7891
rahimuj570 <rahimujjaman2002@gmail.com>
2025-04-14 21:05:14 +0600

    server created and learnd who packeges works🐱‍👤

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/rahimuj570/go_http_jwt_role_based_practice
+
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/rahimuj570/go_http_jwt_role_based_practice/controllers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", controllers.Test)
+
+	http.ListenAndServe(":8080", mux)
+}
